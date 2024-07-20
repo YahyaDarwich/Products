521ba2b592b3227a2f5a60e27a9bb31455b3d12a
YahyaDarwich <ydarwich18@gmail.com>
2024-07-20 17:13:17 +0300

    add delete all products function

--- app/src/main/java/com/example/products/data/ProductsRepository.kt
+++ app/src/main/java/com/example/products/data/ProductsRepository.kt
@@ -1,14 +1,17 @@
 package com.example.products.data
 
 import android.database.Cursor
+import androidx.sqlite.db.SupportSQLiteQuery
 import kotlinx.coroutines.flow.Flow
 
 interface ProductsRepository {
     fun getAllProducts(): Flow<List<Product>>
+    fun insertDataRawFormat(query: SupportSQLiteQuery): Boolean?
     fun getAllProductsToExport(): Cursor
     fun searchProducts(searchKeyword: String): Flow<List<Product>>
     fun getProduct(productId: Int): Flow<Product>
     suspend fun addProduct(product: Product)
     suspend fun updateProduct(product: Product)
     suspend fun deleteProduct(product: Product)
+    suspend fun deleteAllProducts()
 }
